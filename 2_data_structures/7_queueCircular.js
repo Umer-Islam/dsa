a7e2444886f0d0af20281e91ffd895fc8da7c6a1
sisyphys_666 <139012668+Umer-Islam@users.noreply.github.com>
2025-03-13 04:06:57 +0500

    init circular queue

---
+++ 2_data_structures/7_queueCircular.js
@@ -0,0 +1,18 @@
+class CircularQueue {
+  constructor(capacity) {
+    this.items = new Array[capacity]();
+    this.capacity = capacity;
+    this.currentLength = 0;
+    this.front = -1;
+    this.rear = -1;
+  }
+  isFull() {
+    return this.currentLength == this.capacity;
+  }
+  isEmpty() {
+    return this.currentLength === 0;
+  }
+  enqueue(){
+    
+  }
+}
